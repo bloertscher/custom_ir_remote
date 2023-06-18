43f8fc7cdad8fa2ab6f8b3f82c31b548390036c6
Brett Loertscher <loertscher.b@outlook.com>
2023-06-17 21:35:39 -0400

    Add button to test send single command. Successfully translate play and pause

--- custom_ir_remote.ino
+++ custom_ir_remote.ino
@@ -1,8 +1,8 @@
 /*
  * custom_ir_remote.ino
 
- Since my old AV receiver remote doesn't support some of my newer hardware, I'll use the Arduino 
- as a translator. 
+ Since my old AV receiver remote doesn't support some of my newer hardware, I'll use the Arduino
+ as a translator.
  */
 
 #include <Arduino.h>
@@ -33,17 +33,92 @@
 // The main library. Make sure the pin definitions happen before this I think.
 #include <IRremote.hpp>
 
+const int SEND_BUTTON_PIN = APPLICATION_PIN;
+const uint16_t TRANSLATION_ERROR = 0xffff;
+
+unsigned long lastButtonPress = 0;
+
 void setup() {
   Serial.begin(115200);
   // Just to know which program is running on my Arduino
   Serial.println(F("START " __FILE__ " from " __DATE__ "\r\nUsing IRremote library version " VERSION_IRREMOTE));
 
+  // Use this pin to send a signal
+  pinMode(SEND_BUTTON_PIN, INPUT_PULLUP);
+
   // Start the receiver and if not 3. parameter specified, take LED_BUILTIN pin from the internal boards definition as default feedback LED
   IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK);
+  IrSender.begin();
 
   Serial.print(F("Ready to receive IR signals of protocols: "));
   printActiveIRProtocols(&Serial);
   Serial.println(F("at pin " STR(IR_RECEIVE_PIN)));
+}
+
+void loop() {
+  /*
+  * Check if received data is available and if yes, try to decode it.
+  * Decoded result is in the IrReceiver.decodedIRData structure.
+  *
+  * E.g. command is in IrReceiver.decodedIRData.command
+  * address is in command is in IrReceiver.decodedIRData.address
+  * and up to 32 bit raw data in IrReceiver.decodedIRData.decodedRawData
+  */
+  if (IrReceiver.decode()) {
+
+    /*
+    * Print a short summary of received data
+    */
+    IrReceiver.printIRResultShort(&Serial);
+    IrReceiver.printIRSendUsage(&Serial);
+    if (IrReceiver.decodedIRData.protocol == UNKNOWN) {
+      Serial.println(F("Received noise or an unknown (or not yet enabled) protocol"));
+      // We have an unknown protocol here, print more info
+      IrReceiver.printIRResultRawFormatted(&Serial, true);
+    }
+
+
+    // If the command was intended for the DVD player, translate it from the dummy Onkyo DVD code to the Samsung/LG Blu ray player code
+    static const uint16_t dummy_onkyo_dvd = 0x6cd2;
+    static const uint16_t my_lg_blu_ray_address = 0x2d2d;
+    static const int n_repeats = 3;
+
+    if (IrReceiver.decodedIRData.address == dummy_onkyo_dvd) {
+      const uint16_t tx_command = translate_DVD_command(IrReceiver.decodedIRData.command);
+
+      if (tx_command != TRANSLATION_ERROR) {
+        Serial.print(F("Sending command to blu ray player: "));
+        Serial.print(my_lg_blu_ray_address, HEX);
+        Serial.print(" ");
+        Serial.println(tx_command, HEX);
+        Serial.flush();
+
+        IrReceiver.stop();
+        IrSender.sendSamsung(my_lg_blu_ray_address, tx_command, n_repeats);
+        delay(100);
+        IrReceiver.start();
+      }
+    }
+
+    Serial.println();
+
+    /*
+    * !!!Important!!! Enable receiving of the next value,
+    * since receiving has stopped after the end of the current received data packet.
+    */
+    IrReceiver.resume();  // Enable receiving of the next value
+  }
+
+  // Check button for manually sending a command
+  if ((digitalRead(SEND_BUTTON_PIN) == LOW) && (millis() - lastButtonPress > 1000)) {
+    lastButtonPress = millis();
+    Serial.println(F("Sending pause command"));
+    Serial.flush();
+    IrReceiver.stop();
+    // force send a pause command
+    IrSender.sendSamsung(0x2d2d, 0x38, 0);
+    IrReceiver.start();
+  }
 }
 
 uint16_t translate_DVD_command(const uint16_t received_command) {
@@ -71,55 +146,11 @@ uint16_t translate_DVD_command(const uint16_t received_command) {
           tx_command = 0x35;
           break;
         default:
-          tx_command = 0xffff;
+          tx_command = TRANSLATION_ERROR;
           Serial.print(F("Unsupported DVD player command: "));
           Serial.println(received_command, HEX);
           break;
       }
-}
 
-void loop() {
-  /*
-  * Check if received data is available and if yes, try to decode it.
-  * Decoded result is in the IrReceiver.decodedIRData structure.
-  *
-  * E.g. command is in IrReceiver.decodedIRData.command
-  * address is in command is in IrReceiver.decodedIRData.address
-  * and up to 32 bit raw data in IrReceiver.decodedIRData.decodedRawData
-  */
-  if (IrReceiver.decode()) {
-
-    /*
-    * Print a short summary of received data
-    */
-    IrReceiver.printIRResultShort(&Serial);
-    IrReceiver.printIRSendUsage(&Serial);
-    if (IrReceiver.decodedIRData.protocol == UNKNOWN) {
-      Serial.println(F("Received noise or an unknown (or not yet enabled) protocol"));
-      // We have an unknown protocol here, print more info
-      IrReceiver.printIRResultRawFormatted(&Serial, true);
-    }
-    
-
-    /*
-    * !!!Important!!! Enable receiving of the next value,
-    * since receiving has stopped after the end of the current received data packet.
-    */
-    IrReceiver.resume();  // Enable receiving of the next value
-
-    // If the command was intended for the DVD player, translate it from the dummy Onkyo DVD code to the Samsung/LG Blu ray player code
-    static const uint16_t dummy_onkyo_dvd = 0x6cd2;
-    static const uint16_t my_lg_blu_ray_address = 0x2d2d;
-    static const int n_repeats = 3;
-
-    if (IrReceiver.decodedIRData.address == dummy_onkyo_dvd) {
-      const uint16_t tx_command = translate_DVD_command(IrReceiver.decodedIRData.command);
-      
-      // TODO: send the command with IrSender
-      IrSender.sendSamsung(my_lg_blu_ray_address, tx_command, n_repeats);
-
-    } 
-
-    Serial.println();
-  }
+    return tx_command;
 }
